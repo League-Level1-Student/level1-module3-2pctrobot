ca949b43aacebefc51f7995c638b28f5a0b8151d
Hailin Day <haililnday@gmail.com>
2024-03-10 11:28:18 -0700

    Cars and Frogs

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -5,8 +5,9 @@ import processing.core.PApplet;
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    int frogX = 370;
-    int frogY = 570;
+    int frogX = 400;
+    int frogY = 600;
+
     
     @Override
     public void settings() {
@@ -15,7 +16,8 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-
+    	Car car1 = new Car(300, 0, 60, 500);
+    	
     }
 
     @Override
@@ -23,6 +25,7 @@ public class Frogger extends PApplet {
     	background(143, 127, 124);
     	fill(0, 200, 13);
     	ellipse(frogX,frogY,30,30);
+    	
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
@@ -30,19 +33,19 @@ public class Frogger extends PApplet {
     public void keyPressed()
     {
         if(key == CODED){
-            if(keyCode == UP && frogY>=0)
+            if(keyCode == UP && frogY>=30)
             {
                 frogY-=10;
             }
-            else if(keyCode == DOWN && frogY<=600)
+            else if(keyCode == DOWN && frogY<=570)
             {
             	frogY+=10; 
             }
-            else if(keyCode == RIGHT && frogX<=800)
+            else if(keyCode == RIGHT && frogX<=770)
             {
             	frogX+=10;
             }
-            else if(keyCode == LEFT && frogX>=0)
+            else if(keyCode == LEFT && frogX>=30)
             {
             	frogX-=10;
             }
